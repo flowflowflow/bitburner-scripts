312aea1d77500fb05534cb71538a5fcdef25f082
flowflowflow <florian.riedlinger@live.de>
2022-01-02 01:16:42 +0100

    2

--- deep-setup.js
+++ deep-setup.js
@@ -2,34 +2,68 @@
 // TODO: unused + unfinished, based on setup.ns
 export async function main(ns) {
 	const home = "home";
+	const hostMachine = ns.getHostname();
 	const target = ns.args[0];
-	var servers = ns.scan(home);
-	var scripts = ns.ls(home, "basic-");
+	var servers = ns.scan(hostMachine);
+	var scripts = ns.ls(home, ".ns");
 
 	for (let entry in scripts) {
 		ns.tprint("found script: " + scripts[entry]);
 	}
 
-	// Hack servers if needed and populate with scripts
-	for (let entry in servers) {
+	ns.tprint("Host:  " + hostMachine);
+	for (let entry in servers) {	// depth 0
 		ns.tprint("found server: " + servers[entry]);
-		let subservers = ns.scan(servers[entry]);
+		let subservers = ns.scan(servers[entry]);	// depth 1
 		for (let subentry in subservers) {
-			if (subservers[subentry] !== "home") {
-				ns.tprint("----->   " + subservers[subentry]);
-				if (!ns.hasRootAccess(subservers[subentry])) {
-					takeover(subservers[subentry]);
+			if (subservers[subentry] !== home && subservers.length > 1) {
+				ns.tprint("-----> " + subservers[subentry]);
+				await takeover(subservers[subentry]);
+				let subserversd2 = ns.scan(subservers[subentry]); // depth 2
+				for (let subentryd2 in subserversd2) {
+					if (subserversd2[subentryd2] !== subservers[subentry] && subserversd2[subentryd2] !== servers[entry] && subserversd2.length > 1) {
+						ns.tprint("----------> " + subserversd2[subentryd2]);
+						await takeover(subserversd2[subentryd2]);
+						let subserversd3 = ns.scan(subserversd2[subentryd2]);	// depth 3
+						for (let subentryd3 in subserversd3) {
+							if (subserversd3[subentryd3] !== subserversd2[subentryd2] && subserversd3[subentryd3] !== subservers[subentry] && subserversd3.length > 1) {
+								ns.tprint("----------------> " + subserversd3[subentryd3]);
+								await takeover(subserversd3[subentryd3]);
+								let subserversd4 = ns.scan(subserversd3[subentryd3]);	// depth 4
+								for (let subentryd4 in subserversd4) {
+									if (subserversd4[subentryd4] !== subserversd3[subentryd3] && subserversd4[subentryd4] !== subserversd2[subentryd2] && subserversd4.length > 1) {
+										ns.tprint("----------------------> " + subserversd4[subentryd4]);
+										await takeover(subserversd4[subentryd4]);
+										let subserversd5 = ns.scan(subserversd4[subentryd4]);	// depth 5
+										for (let subentryd5 in subserversd5) {
+											if (subserversd5[subentryd5] !== subserversd4[subentryd4] && subserversd5[subentryd5] !== subserversd3[subentryd3] && subserversd5.length > 1) {
+												ns.tprint("-------------------------> " + subserversd5[subentryd5]);
+												await takeover(subserversd5[subentryd5]);
+												let subserversd6 = ns.scan(subserversd5[subentryd5]);	// depth 6
+												for (let subentryd6 in subserversd6) {
+													if (subserversd6[subentryd6] !== subserversd5[subentryd5] && subserversd6[subentryd6] !== subserversd4[subentryd4] && subserversd6.length > 1) {
+														ns.tprint("------------------------------> " + subserversd6[subentryd6]);
+														await takeover(subserversd6[subentryd6]);
+														let subserversd7 = ns.scan(subserversd6[subentryd6]);	// depth 7
+														for (let subentryd7 in subserversd7) {
+															if (subserversd7[subentryd7] !== subserversd6[subentryd6] && subserversd7[subentryd7] !== subserversd5[subentryd5] && subserversd7.length > 1) {
+																ns.tprint("-----------------------------------> " + subserversd7[subentryd7]);
+																await takeover(subserversd7[subentryd7]);
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
 				}
-				ns.tprint("copying scripts to " + subservers[subentry]);
-				await ns.scp(scripts, home, subservers[subentry]);
 			}
 		}
-
-		if (!ns.hasRootAccess(servers[entry])) {
-			takeover(servers[entry]);
-		}
-		ns.tprint("copying scripts to " + servers[entry]);
-		await ns.scp(scripts, home, servers[entry]);
+		await takeover(servers[entry]);
 	}
 
 	/** 
@@ -43,7 +77,7 @@ export async function main(ns) {
 		let isNotTakenOver = true;
 		let attemptCounter = 0;
 
-		if (ns.getServerNumPortsRequired <= 5 && attemptCounter < 30) {
+		if (/*ns.getServerNumPortsRequired <= 5 && */ attemptCounter < 30) {
 			while (isNotTakenOver) {
 				if (ns.fileExists("BruteSSH.exe", "home")) {
 					ns.brutessh(currentTarget);
@@ -61,13 +95,19 @@ export async function main(ns) {
 					ns.sqlinject(currentTarget);
 				}
 				if (ns.fileExists("NUKE.exe", "home")) {
-					ns.nuke(currentTarget);
-					isNotTakenOver = !ns.hasRootAccess(currentTarget);
+					try {
+						ns.nuke(currentTarget);
+						isNotTakenOver = !ns.hasRootAccess(currentTarget);
+					} catch(error) {
+						ns.tprint("not enough ports to nuke " +  currentTarget);
+						return false;
+					}
+
 				}
-				
 				attemptCounter++;
 			}
 
+			await ns.scp(scripts, home, currentTarget);
 			ns.tprint("target " + currentTarget + " taken over");
 			return true;
 		}
